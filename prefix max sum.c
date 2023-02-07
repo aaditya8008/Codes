510715444af8ee14ad2dd9ed9d47b74003c7f6e9
Aditya saini <aadityasaini8008@gmail.com>
2023-02-07 13:21:09 +0530

    prefix max modified

---
+++ prefix max sum.c
@@ -0,0 +1,39 @@
+import java.util.*;
+public class rogh{
+    public static void psum(int n,int arr[]){
+        int sum=0,mxsum=Integer.MIN_VALUE;
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i]<arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+     }
+     for(int i=0;i<n;i++){
+        System.out.print(arr[i]);
+        }
+     
+
+     }
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Enter elements:\n");
+        
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]);
+            }
+            System.out.print("\n");
+            
+
+            psum(n,arr);
+            
+    }}

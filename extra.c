86874be5e8fc5b41869eb370ead4a24e4ea0e03d
aaditya8008 <114769448+aaditya8008@users.noreply.github.com>
2023-03-19 15:03:46 +0530

    saved

--- extra.c
+++ extra.c
@@ -1,63 +1,78 @@
 #include<stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 struct node{
 int data;
-struct node*next;
-}*n1,*n2,*n3,*head,*head1,*head2,*n,*merg;
-struct node *insert(struct node *n){
- 
+struct node* next;
+}*head,*head1,*head2,*head3,*n,*new,*prev,*n1,*n2,*n3;
+struct node *create(struct node*n,int num,int i){
 n=(struct node*)malloc(sizeof(struct node));
-
 scanf("%d",&n->data);
-n->next=head;
-head=n;
+if(i==0){
+    head=n;
+    prev=head;
+}
+prev->next=n;
+n->next=NULL;
+prev=n;
+
 return head;
 }
+struct node *merge(struct node *head1,struct node *head2){
+n1=head1;
+n2=head2;
 
-struct node *merge(struct node*head1,struct node *head2,struct node*merg){
-    n1=head1;
-    n2=head2;
-    
-    
-    while(n1!=NULL&&n2!=NULL){
-        n3=(struct node*)malloc(sizeof(struct node));
-if(n1->data<=n2->data){
-n3->data=n1->data;
-n1=n1->next;
+int i=0;
+while(1){
+n3=(struct node*)malloc(sizeof(struct node));
+if(i==0){
+    head3=n3;
+    prev=head3;
 }
-if(n1->data >= n2->data){
+if(n1==NULL){
+n3->data=n2->data;
+//n2=n2->next;
+break;
+}
+if(n2==NULL){
+n3->data=n1->data;
+//n1=n1->next;
+break;
+}
+if(n2->data<=n1->data){
 n3->data=n2->data;
 n2=n2->next;
 }
-n3->next=merg;
-merg=n3;
+else if(n1->data<n2->data){
+n3->data=n1->data;
+n1=n1->next;
 }
-return merg;
+
+prev->next=n3;
+n3->next=NULL;
+prev=n3;
+i++;
 }
 
-
-
-
+return head3;
+}
 int main(){
-for(int i=0;i<5;i++)
-head1=insert(n1);
-for(int i=0;i<5;i++){
-    printf("%d",head1->data);
-    head1=head1->next;
-}
-for(int i=0;i<5;i++)
-head2=insert(n2);
-for(int i=0;i<5;i++){
-    printf("%d",head2->data);
-    head2=head2->next;
-}
-
-
-merg=NULL;
-
-merg=merge(head1,head2,merg);
-for(int i=0;i<10;i++){
-    printf("%d\n",merg->data);
-    merg=merg->next;
-}
+    int num;
+    printf("Enter size :\n");
+    scanf("%d",&num);
+    printf("Enter data of Linked list :\n");
+    
+    for(int i=0;i<num;i++){
+        create(n,num,i);
+    }head1=head;
+    printf("Enter data of Linked list :\n");
+    for(int i=0;i<num;i++){
+        create(n,num,i);
+    }head2=head;
+    head3=merge(head1,head2);
+    printf("Merged list :\n");
+    while(head3!=NULL){
+    printf("%d\n",head3->data);
+    head3=head3->next;}
+    
+    
 }

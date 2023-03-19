1351fc5314242566127fda1fa10be7a660909cfa
aaditya8008 <114769448+aaditya8008@users.noreply.github.com>
2023-03-20 00:33:53 +0530

    AS

--- rough.c
+++ rough.c
@@ -1,10 +1,46 @@
 #include<stdio.h>
-#include<string.h>
-
-int main(){
-char str[10],str2[10];
-fgets(str,10,stdin);
-printf("%d\n",strlen(str));
-char *p=str;
-
+#include<stdlib.h>
+struct node{
+int num;
+struct node *next;         
+}*n1,*n2,*n3,*n4,*p,*q;
+int cycle(struct node *head){
+    int exist=0;
+p=q=head;
+while(q&&q->next){
+p=p->next;
+q=q->next->next;
+if(q==p){
+    printf("Cycle\n");
+    exist=1;
+    break;
 }
+
+}if(exist){
+p=head;
+while(p!=q){
+     
+    p=p->next;
+    q=q->next;
+}
+printf("data=%d",p->num);
+}
+else{
+    printf("No loop exists\n");
+return 0;}
+return 1;
+}
+int main(){
+n1=(struct node*)malloc(sizeof(struct node));
+n2=(struct node*)malloc(sizeof(struct node));
+n3=(struct node*)malloc(sizeof(struct node));
+n4=(struct node*)malloc(sizeof(struct node));
+printf("Enter Data :\n");
+scanf("%d%d%d%d",&n1->num,&n2->num,&n3->num,&n4->num);
+n1->next=n2;
+n2->next=n3;
+n3->next=n4;
+n4->next=n2;
+cycle(n1);
+}
+
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,50 +1,19 @@
 #include<stdio.h>
-#include<stdlib.h>
-struct node{
-int num;
-struct node *next;         
-}*n1,*n2,*n3,*n4,*p,*q;
-int cycle(struct node *head){
-    int exist=0,count;
-p=q=head;
-while(q&&q->next){
-p=p->next;
-q=q->next->next;
-if(q==p){
-    printf("Cycle\n");
-    exist=0;
-    break;
-}
-count=1;
-}if(1){
+struct student{
+char name[10];
+int rollno;
+};
 
-while(q&&q->next){
-    q=q->next;
-    if(p==q){
-        break;
-    }
-    
-    count++;
-    
-}
-printf("count=%d",count);
-}
-else{
-    printf("No loop exists\n");
-return 0;}
-return 1;
-}
 int main(){
-n1=(struct node*)malloc(sizeof(struct node));
-n2=(struct node*)malloc(sizeof(struct node));
-n3=(struct node*)malloc(sizeof(struct node));
-n4=(struct node*)malloc(sizeof(struct node));
-printf("Enter Data :\n");
-scanf("%d%d%d%d",&n1->num,&n2->num,&n3->num,&n4->num);
-n1->next=n2;
-n2->next=n3;
-n3->next=n4;
-n4->next=n2;
-cycle(n1);
-}
+    printf("Enter size:\n");
+    int n;
+    scanf("%d",&n);
+    struct student s[n];
+    for(int i=0;i<n;i++){
+printf("Enter Data of student :%d\n",i);
+   scanf("%s%d",s[i].name,&s[i].rollno);
+    }
+for(int i=0;i<n;i++)
+printf("%s\t%d\n",s[i].name,s[i].rollno);
 
+}
